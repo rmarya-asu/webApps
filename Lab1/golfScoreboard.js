fcfa5a242f55aefa26d08f73c942adccd7a6573f
ruthar <rmarya@asu.edu>
2017-09-11 22:22:12 -0700

    changes to the code - constructor is accepting the json string to construct object

--- Lab1/golfScoreboard.js
+++ Lab1/golfScoreboard.js
@@ -8,38 +8,105 @@ function Player(lastname, firstinitial, score, hole) {
   this.sayName = function() {
     console.log(this.lastname + " " + this.firstinitial);
   }
+
+  this.getHole = function() {
+    if (this.hole === 'finished') {
+      console.log("making the value 18");
+      return 18;
+    } else {
+      return this.hole
+    }
+  }
 }
 
-function Tournament(name, year, award, yardage, par, round, players) {
-  this.name = name;
-  this.year = year;
-  this.yardage = yardage;
-  this.par = par;
-  this.round = round;
-  this.players = players;
+function Tournament(input) {
+  var parsedData = JSON.parse(input);
+  var tour = parsedData.tournament;
+  var players = parsedData.tournament.players;
+  var playersArray = new Array();
 
-  this.printTournament = function() {
+  for (x in players) {
+    var playerObject = new Player(players[x].lastname, players[x].firstinitial, players[x].score, players[x].hole);
+    playersArray.push(playerObject);
+  }
+
+  this.name = tour.name;
+  this.year = tour.year;
+  this.yardage = tour.yardage;
+  this.par = tour.par;
+  this.round = tour.round;
+  this.players = playersArray;
+
+  this.printTournamentName = function() {
     console.log(this.name);
   }
 
   this.leaderboard = function() {
-    players.sort(function(a, b) {
-      return a.score - b.score
-    })
-    console.log(players);
+    console.log("this.players " + this.players);
+    this.players.sort(function(a, b) {
+      return a.score - b.score;
+    });
+    //TODO: clhow to sort it next by hole?
+    return JSON.stringify(this.players);
+  }
+
+  this.findPlayer = function(lastname, firstinitial) {
+    console.log("Finding player with name " + lastname);
+    for (x in this.players) {
+      if (players[x].lastname === lastname && players[x].firstinitial === firstinitial) {
+        return x;
+      }
+    }
+    //player not found
+    return null;
+  }
+
+  this.getAverageOfAllScores = function() {
+    console.log("average of ");
+    var totalScore = 0;
+    var totalHoles = 18 * (this.round - 1);
+    console.log("round = " + this.round + "total holes :" + totalHoles);
+    var i = 0;
+    for (x in this.players) {
+      var playerHole = this.players[x].getHole();
+      var scorebyHole = this.players[x].score / (totalHoles + playerHole);
+      totalScore += scorebyHole;
+      i++;
+    }
+    return (totalScore) / i;
   }
 
   this.projectScoreByIndividual = function(lastname, firstinitial) {
-    for (x in this.players) {
-      if (players[x].lastname === lastname && players[x].firstinitial === firstinitial) {
-        console.log(players[x].lastname);
-        //TODO: handle error case for 'hole can be finished'
-        var scorePerHole = players[x].score / players[x].hole;
-        var projectedScore = (scorePerHole * (18 - players[x].hole)) + players[x].score;
-        return projectedScore;
-      }
+    console.log("inside project score by individual");
+    var playerIndex = this.findPlayer(lastname, firstinitial);
+    if (!playerIndex) {
+      //what do i return if player is not found?
+      return console.log("player with name " + lastname + " " + firstinitial + " not found!");
     }
-    console.log("player with lastname :" + lastname + " and firstinitial :" + firstinitial + " not found");
+    console.log(this.players[playerIndex].lastname);
+    //TODO: handle error case for 'hole can be finished' - done, added getHole method to convert -> "finised to 18"
+    var scorePerHole = this.players[playerIndex].score / this.players[playerIndex].getHole();
+    //TODO: I have assumed that 18 is the hole for the particular game.
+    console.log("score per hole : " + scorePerHole);
+    var projectedScore = (scorePerHole * (18 - this.players[playerIndex].getHole())) + players[playerIndex].score;
+    return math.round(projectedScore);
+  }
+
+  this.projectScoreByHole = function(lastname, firstinitial) {
+    console.log("project by hole");
+    var playerIndex = this.findPlayer(lastname, firstinitial);
+    if (!playerIndex) {
+      //what do i return if player is not found?
+      return console.log("player with name " + lastname + " " + firstinitial + " not found!");
+    }
+    console.log("got the player");
+    console.log(players[playerIndex].lastname);
+    //TODO: handle error case for 'hole can be finished'
+    var averageScore = this.getAverageOfAllScores();
+    var projectedScoreByH = this.players[playerIndex].score + ((18 - this.players[playerIndex].getHole()) * averageScore);
+    console.log(averageScore);
+    console.log(projectedScoreByH);
+    return Math.round(projectedScoreByH);
   }
 
   this.isRoundCompleted = function() {
@@ -79,26 +146,19 @@ var inputData = {
 
 var inputStringData = JSON.stringify(inputData);
 
-function golfParser(input) {
-  var parsedData = JSON.parse(input);
-  var tour = parsedData.tournament;
-  var players = parsedData.tournament.players;
-  var playersArray = new Array();
-  for (x in players) {
-    var playerObject = new Player(players[x].lastname, players[x].firstinitial, players[x].score, players[x].hole);
-    playersArray.push(playerObject);
-  }
-  return new Tournament(tour.name, tour.year, tour.award, tour.yardage, tour.par, tour.round, playersArray);
-}
-
 function testcase1(inputData) {
   console.log("running test case 1 : check type of object passed into parser and log the returned data (parsed json into a js object)")
-  var tournamentObject = golfParser(inputData);
-  console.log(tournamentObject.constructor)
-  console.log(tournamentObject.name);
-  console.log(tournamentObject.players[0].sayName());
-  tournamentObject.leaderboard();
-  console.log(tournamentObject.isRoundCompleted());
-  console.log(tournamentObject.projectScoreByIndividual("Montgomerie", "C"));
+  var tournamentObject = new Tournament(inputData);
+  // //console.log("constructor: "+tournamentObject.constructor)
+  // console.log("tournament Name: "+tournamentObject.name);
+  // tournamentObject.players[0].sayName();
+  // console.log("the leaderboard "+tournamentObject.leaderboard());
+  // console.log("is it completed? "+tournamentObject.isRoundCompleted());
+  //console.log("project score of Montgomerie :" + tournamentObject.projectScoreByIndividual("Montgomerie", "C"));
+  //console.log("project score of Montgomerie :" + tournamentObject.projectScoreByIndividual("Mont", "C"));
+  //console.log("project score of Fulke :" + tournamentObject.projectScoreByIndividual("Fulke", "P"));
+  console.log("project score of Mont :" + tournamentObject.projectScoreByHole("Montgomerie", "C"));
+  // tournamentObject.projectScoreByHole("Montgomerie", "C");
 }
+
 testcase1(inputStringData);

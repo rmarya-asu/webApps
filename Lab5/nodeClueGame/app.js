f0b1ba89f9c860471c0db4202ab771871c513959
rmarya-asu <ruthvik.arya@asu.edu>
2017-11-08 14:54:50 -0700

    bootstrapping nodeclue game - removing jade and plugging in ejs

--- Lab5/nodeClueGame/app.js
+++ Lab5/nodeClueGame/app.js
@@ -6,13 +6,12 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,5 +40,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+app.listen(3000,()=>{
+  console.log('express server listening on port 3000');
+});
 module.exports = app;

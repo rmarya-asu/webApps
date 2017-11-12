c9e63cb81602f8057e9d0136017ace23b69a2123
rmarya-asu <ruthvik.arya@asu.edu>
2017-11-12 10:08:02 -0700

    adding bootstrap and background to the table

--- Lab5/angularWeatherApp/src/app/weather-table/weather-table.component.ts
+++ Lab5/angularWeatherApp/src/app/weather-table/weather-table.component.ts
@@ -7,17 +7,44 @@ import {WeatherDataService} from '../shared/weather-data.service';
   styleUrls: ['./weather-table.component.css']
 })
 export class WeatherTableComponent implements OnInit {
-  _weatherData: any;
+  // weatherData is an array of weather ffom cities. NEED an interface to define type of the data I"m holding
+  _weatherData: ICity[];
   errorMessage: string;
   constructor(private _weatherDataService: WeatherDataService) { }
 
   ngOnInit() {
     this._weatherDataService.getWeatherData().subscribe(data => {
-      this._weatherData = data;
+      // const city = new ICity(data.name, data.sys.country, data.dt, data.main.temp, data.main.humidity, data.wind.speed, data.couds.all);
+      let city: ICity;
+      city.name = data.name;
+      city.country = data.sys.country;
+      city.time = data.dt;
+      city.temp = data.main.temp;
+      city.humidity = data.main.humidity;
+      city.windSpeed = data.wind.speed;
+      city.clouds = data.couds.all;
+      this._weatherData.push(city);
     } , error => this.errorMessage = <any> error);
   }
+  // parseWeatherData = function(data, next){
+  //
+  // }
   func() {
     console.log(this._weatherData);
   }
 
 }
+
+export interface ICity {
+  name: string;
+  country: string;
+  time: string;
+  temp: number;
+  humidity: number;
+  windSpeed: number;
+  clouds: number;
+}
+
+const WEATHER_DATA: ICity[] = [
+  {name: 'bangalore', country: 'Hydrogen', time: '241240079', temp: 23, humidity: 13, windSpeed: 34, clouds: 23},
+  {name: 'bangalore', country: 'Hydrogen', time: '241240079', temp: 23, humidity: 13, windSpeed: 4, clouds: 23}];
--- Lab5/nodeClueGame/routes/index.js
+++ Lab5/nodeClueGame/routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CLUE' });
 });
 
+router.get('/test',function(req,res,next){
+  res.render('test');
+})
+
+
+router.post('/guess',function(req,res,next){
+  console.log(req.params.body);
+  next();
+});
+
+
+router.get('/scores',function(req,res,next){
+
+})
 module.exports = router;
